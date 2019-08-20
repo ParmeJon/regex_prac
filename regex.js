5f72d9cb129615af0eae68485ca45cd8740a998f
github username <jonathansychan@gmail.com>
2019-08-20 19:29:52 -0400

    Match single character not specified

--- regex.js
+++ regex.js
@@ -25,7 +25,8 @@ console.log(result3)
 //   groups: undefined
 // ]
 
-// Finding more than the first Match by adding 'g' flag
+// Finding more than the first Match by adding 'g' flag 
+// first match is the default
 let testStr = "Repeat, Repeat, Repeat"
 let ourRegex = /Repeat/g
 let result4 = testStr.match(ourRegex);
@@ -39,3 +40,71 @@ let result5 = twinkleStar.match(starRegex)
 console.log(result5)
 // logs [ 'Twinkle', 'twinkle' ]
 
+// Wildcard Period 
+// Can be added at the front or back /.um/ or /hu./
+let humStr = "I'll hum a song"
+let hugStr = "Bear hug"
+let huRegex = /hu./
+let result6 = humStr.match(huRegex)
+let result7 = hugStr.match(huRegex)
+console.log(result6, result7)
+
+// Match Single Character with multiple possibilities.
+let bgRegex = /b[aiu]g/
+// matches bag big bug
+
+// this will grab all the vowels in quoteSample.
+let quoteSample = "Beware of bugs in the above code; i have only proved it correctly once."
+let vowelRegex = /[aeiou]/ig
+let result8 = quoteSample.match(vowelRegex)
+console.log(result8)
+// logs [
+//   'e', 'a', 'e', 'o',
+//   'u', 'i', 'e', 'a',
+//   'o', 'e', 'o', 'e',
+//   'i', 'a', 'e', 'o',
+//   'o', 'e', 'i', 'o',
+//   'e', 'o', 'e'
+// ]
+
+// Match letters of the alphabet
+let quoteSample2 = "The quick brown fox jumps over the lazy dog."
+let alphabetRegex = /[a-z]/ig
+let result9 = quoteSample2.match(alphabetRegex)
+console.log(result9)
+// logs [
+//   'T', 'h', 'e', 'q', 'u',
+//   'i', 'c', 'k', 'b', 'r',
+//   'o', 'w', 'n', 'f', 'o',
+//   'x', 'j', 'u', 'm', 'p',
+//   's', 'o', 'v', 'e', 'r',
+//   't', 'h', 'e', 'l', 'a',
+//   'z', 'y', 'd', 'o', 'g'
+// ]
+
+// Match numbers and letters 
+let quoteSample3 = "Blueberry 3.141592653s are delicious."
+let azNumRegex = /[2-6h-s]/ig
+let result10 = quoteSample3.match(azNumRegex)
+console.log(result10)
+// logs [
+//   'l', 'r', 'r',
+//   '3', '4', '5',
+//   '2', '6', '5',
+//   '3', 's', 'r',
+//   'l', 'i', 'i',
+//   'o', 's'
+// ]
+
+// Match single character not specified
+// match everything except all numbers and all vowels
+let quoteSample4 = "3 blind mince."
+let myRegex2 = /[^0-9aeiou]/ig
+let result11 = quoteSample4.match(myRegex2)
+console.log(result11)
+// logs [
+//   ' ', 'b', 'l',
+//   'n', 'd', ' ',
+//   'm', 'n', 'c',
+//   '.'
+// ]
